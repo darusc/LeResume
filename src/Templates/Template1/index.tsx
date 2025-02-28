e5616a9b6e17af6a0742adb39cd0490a19cb4fc5
hypertensiune <dariuscirneala05@gmail.com>
2025-02-28 19:24:05 +0200

    Removed the template component system

--- src/Templates/Template1/index.tsx
+++ src/Templates/Template1/index.tsx
@@ -1,53 +1,146 @@
-import { Col, EducationI, Header, IconText, Line, Main, Project, Resume, SectionI, SectionL, Text, Work, IconLink } from '../components';
+import { Language, parseDate, IconText, IconLink } from '../components';
+
 import './t1.scss';
 
-const Template1 = ({ mref, scale, data }: { mref: any, scale: number, data: ResumeData }) => {
+export default function Template1({ mref, scale, data }: { mref: any, scale: number, data: ResumeData }) {
   return (
-    <>
-      <Resume id='t1' mref={mref} scale={scale}>
-        <Header name={data.basics.firstname + " " + data.basics.lastname}>
-          <SectionI>
-            {data.basics.phone && <IconText icon='fa-solid fa-phone' text={data.basics.phone} />}
-            {data.basics.email && <IconText icon='fa-solid fa-envelope' text={data.basics.email} />}
-            {data.basics.github && <IconLink icon='fa-brands fa-github' text='Github' link={"https://" + data.basics.github} />}
-            {data.basics.linkedin && <IconLink icon='fa-brands fa-linkedin' text='LinkedIn' link={"https://" + data.basics.linkedin} />}
-            {data.basics.location && <IconText icon='fa-solid fa-location-dot' text={data.basics.location} />}
-          </SectionI>
-        </Header>
-        <Line />
-        <Main>
-          <Col width='100%'>
+    <div ref={mref} id='t1' className="a4 pdf resume" style={{transform: `scale(${scale})`}}>
+      <div className="container">
+        
+        <header>
+          <h1 className="name">{data.basics.firstname + " " + data.basics.lastname}</h1>
+          {data.basics.jobtitle && <h3 className='title'>{data.basics.jobtitle}</h3>}
+          <section className='section inline'>
+            <div className="content">
+              {data.basics.phone && <IconText icon='fa-solid fa-phone' text={data.basics.phone} />}
+              {data.basics.email && <IconText icon='fa-solid fa-envelope' text={data.basics.email} />}
+              {data.basics.github && <IconLink icon='fa-brands fa-github' text='Github' link={"https://" + data.basics.github} />}
+              {data.basics.linkedin && <IconLink icon='fa-brands fa-linkedin' text='LinkedIn' link={"https://" + data.basics.linkedin} />}
+              {data.basics.location && <IconText icon='fa-solid fa-location-dot' text={data.basics.location} />}
+            </div>
+          </section>
+        </header>
+
+        <span className="line"/>
+
+        <main>
+          <div className="col" style={{width: '100%'}}>
+
+            {/* skills */}
             {data.skills.length > 0 && data.skills[0].skills.length > 0 &&
-              <SectionL name='Skills'>
-                {data.skills.map(s => <Text><b>{s.type}:</b> {s.skills}</Text>)}
-              </SectionL>
+              <section className="inline">
+                <h4>Skills</h4>
+                <ul style={{ listStyle: "none", padding: 0 }}>
+                  {data.skills.map(s => (
+                    <li>
+                      <span><b>{s.type}:</b> {s.skills}</span>
+                    </li>
+                  ))}
+                </ul>
+              </section>
             }
+
+            {/* projects */}
             {data.projects.length > 0 && data.projects[0].name &&
-              <SectionL name='Projects'>
-                {data.projects.map(project => <Project project={project} />)}
-              </SectionL>
+              <section className="inline">
+                <h4>Projects</h4>
+                <ul style={{ listStyle: "none", padding: 0 }}>
+                {data.projects.map(project => (
+                  <li>
+                    <div className="project">
+                      <div className="row">
+                        <span className="name">{project.name}</span>
+                      </div>
+                      <div className="row">
+                        <div style={{display: 'flex'}}>
+                          <div style={{paddingRight: '10px'}}>
+                            {project.github && <IconLink text='Github' icon='fa-brands fa-github' link={project.github} />}
+                          </div>
+                          <div>
+                            {project.website && <IconLink text={project.website} icon='fa-solid fa-globe' link={"https://" + project.website} />}
+                          </div>
+                        </div>
+                        <div>
+                          {[...project.languages].map((lang: string) => <Language key={lang} lang={lang} />)}
+                        </div>
+                      </div>
+                      {project.description.length > 0 &&
+                        <ul>
+                          {project.description.map((dsc: string, i) => <li key={i}><span>{dsc}</span></li>)}
+                        </ul>
+                      }
+                    </div>
+                  </li>
+                ))}
+                </ul>
+              </section>
             }
+
+
+            {/* experience */}
             {data.work.length > 0 && data.work[0].position &&
-              <SectionL name='Experience'>
-                {data.work.map(w => <Work work={w} />)}
-              </SectionL>
+              <section className="inline">
+                <h4>Experience</h4>
+                <ul style={{ listStyle: "none", padding: 0 }}>
+                  {data.work.map(work => (
+                    <li>
+                      <div className='work'>
+                        <div className='row'>
+                          <span className='pos'>{work.position}</span>
+                          <span>{work.start && parseDate(work.start) + " - " + ((work.end != undefined && work.end != "") ? parseDate(work.end) : "present")}</span>
+                        </div>
+                        <div className='row'>
+                          <span>{work.company}</span>
+                          <span>{work.location}</span>
+                        </div>
+                        <ul>
+                          {work.description.map((dsc: string) => <li><span>{dsc}</span></li>)}
+                        </ul>
+                      </div>
+                    </li>
+                  ))}
+                </ul>
+              </section>
             }
+
+
+            {/* education  */}
             {data.education.length > 0 && data.education[0].institution &&
-              <SectionL name='Education' spaced>
-                {data.education.map((edu: Education) => <EducationI education={edu} />)}
-              </SectionL>
+              <section className="inline spaced">
+                <h4>Education</h4>
+                <ul style={{ listStyle: "none", padding: 0 }}>
+                  {data.education.map((education: Education) => (
+                    <div className='education list'>
+                      <span className='top'>{education.degree}</span>
+                      <span className='middle'>{education.institution}</span>
+                      <span className='bottom'>{education.start && parseDate(education.start) + " - " + parseDate(education.end)}</span>
+                    </div>
+                  ))}
+                </ul>
+              </section>
             }
-            
+
+
+            {/* certifications */}
             {data.certifications.length > 0 && data.certifications[0].issuer &&
-              <SectionL name='Certifications'>
-                {data.certifications.map((edu: any) => <EducationI education={edu} />)}
-              </SectionL>
+              <section className="inline spaced">
+                <h4>Certifications</h4>
+                <ul style={{ listStyle: "none", padding: 0 }}>
+                  {data.certifications.map((certification: Certification) => (
+                    <div className='education inline'>
+                      <span className='top'>{certification.name}</span>
+                      <div className='flex'>
+                        <span className='middle'>{certification.issuer}</span>
+                        <span className='bottom'>{(certification.date && parseDate(certification.date))}</span>
+                      </div>
+                    </div>
+                  ))}
+                </ul>
+              </section>
             }
-          </Col>
-        </Main>
-      </Resume>
-    </>
+          </div>
+        </main>
+      </div>
+    </div>
   )
 }
-
-export default Template1;
--- src/Templates/Template2/index.tsx
+++ src/Templates/Template2/index.tsx
@@ -1,60 +1,166 @@
-import { Col, Header, IconText, Line, Main, Resume, SectionL, Work, Text, EducationL, Project, IconLink } from '../components';
+import { Language, parseDate, IconText, IconLink } from '../components';
+
 import './t2.scss'
 
 export default function Template2({ mref, scale, data }: { mref: any, scale: number, data: ResumeData }) {
   return (
-    <>
-      <Resume id='t2' mref={mref} scale={scale}>
-        <Header name={data.basics.firstname + ' ' + data.basics.lastname} title={data.basics.jobtitle} />
-        <Main>
-          <Col width='35%' className='left'>
-            <SectionL name='Contact'>
-              {data.basics.phone && <IconText icon='fa-solid fa-phone' text={data.basics.phone} />}
-              {data.basics.email && <IconText icon='fa-solid fa-envelope' text={data.basics.email} />}
-              {data.basics.github && <IconLink icon='fa-brands fa-github' text={data.basics.github} link={"https://" + data.basics.github} />}
-              {data.basics.linkedin && <IconLink icon='fa-brands fa-linkedin' text={data.basics.linkedin} link={"https://" + data.basics.linkedin} />}
-              {data.basics.website && <IconLink icon='fa-solid fa-link' text={data.basics.website} link={"https://" + data.basics.website} />}
-              {data.basics.location && <IconText icon='fa-solid fa-location-dot' text={data.basics.location} />}
-            </SectionL>
+    <div ref={mref} id='t2' className="a4 pdf resume" style={{transform: `scale(${scale})`}}>
+      <div className="container">
+
+
+        {/* header */}
+        <div className="header">
+          <h1 className="name">{data.basics.firstname + " " + data.basics.lastname}</h1>
+          {data.basics.jobtitle && <h3 className='title'>{data.basics.jobtitle}</h3>}
+        </div>
+
+        <div className="main">
+
+          <div className="col left" style={{width: '35%'}}>
+            
+            {/* contact */}
+            <div className="section inline">
+              <h4>Contact</h4>
+              <ul style={{ listStyle: "none", padding: 0 }}>
+                {data.basics.phone && <IconText icon='fa-solid fa-phone' text={data.basics.phone} />}
+                {data.basics.email && <IconText icon='fa-solid fa-envelope' text={data.basics.email} />}
+                {data.basics.github && <IconLink icon='fa-brands fa-github' text={data.basics.github} link={"https://" + data.basics.github} />}
+                {data.basics.linkedin && <IconLink icon='fa-brands fa-linkedin' text={data.basics.linkedin} link={"https://" + data.basics.linkedin} />}
+                {data.basics.website && <IconLink icon='fa-solid fa-link' text={data.basics.website} link={"https://" + data.basics.website} />}
+                {data.basics.location && <IconText icon='fa-solid fa-location-dot' text={data.basics.location} />}
+              </ul>
+            </div>
+            
+
+            {/* skills */}
             {data.skills.length > 0 && data.skills[0].skills.length > 0 &&
               <>
-                <Line />
-                <SectionL name='Skills'>
-                  {data.skills.map(s => <Text key={s.id}>{s.skills}</Text>)}
-                </SectionL>
+                <span className="line"/>
+                <div className="section inline">
+                  <h4>Skills</h4>
+                  <ul style={{ listStyle: "none", padding: 0 }}>
+                    {data.skills.map(s => (
+                      <li>
+                        <span>{s.skills}</span>
+                      </li>
+                    ))}
+                  </ul>
+                </div>
               </>
-            }
+            } 
+
+
+            {/* education  */}
             {data.education.length > 0 && data.education[0].institution &&
               <>
-                <Line />
-                <SectionL name='Education' spaced>
-                  {data.education.map((edu: Education) => <EducationL key={edu.id} education={edu} />)}
-                </SectionL>
+                <span className="line"/>
+                <div className="section inline">
+                  <h4>Education</h4>
+                  <ul className='spaced' style={{ listStyle: "none", padding: 0 }}>
+                    {data.education.map((education: Education) => (
+                      <li>
+                        <div className='education list'>
+                          <span className='top'>{education.degree}</span>
+                          <span className='middle'>{education.institution}</span>
+                          <span className='bottom'>{education.start && parseDate(education.start) + " - " + parseDate(education.end)}</span>
+                        </div>
+                      </li>
+                    ))}
+                  </ul>
+                </div>
               </>
             }
+            
+            {/* certifications  */}
             {data.certifications.length > 0 && data.certifications[0].issuer &&
               <>
-                <Line />
-                <SectionL name='Certifications'>
-                  {data.certifications.map((edu: any) => <EducationL key={edu.id} education={edu} />)}
-                </SectionL>
+                <span className="line"/>
+                <div className="section inline">
+                  <h4>Certifications</h4>
+                  <ul className='spaced' style={{ listStyle: "none", padding: 0 }}>
+                    {data.certifications.map((certification: Certification) => (
+                      <li>
+                        <div className='education list'>
+                          <span className='top'>{certification.name}</span>
+                          <span className='middle'>{certification.issuer}</span>
+                          <span className='bottom'>{parseDate(certification.date)}</span>
+                        </div>
+                      </li>
+                    ))}
+                  </ul>
+                </div>
               </>
             }
-          </Col>
-          <Col width='65%' className='right'>
+          </div>
+
+          <div className="col right" style={{width: '65%'}}>
+
+            {/* projects  */}
             {data.projects.length > 0 && data.projects[0].name &&
-              <SectionL name='Projects'>
-                {data.projects.map(project => <Project key={project.id} project={project} />)}
-              </SectionL>
+              <div className="section inline">
+                <h4>Projects</h4>
+                <ul style={{ listStyle: "none", padding: 0 }}>
+                  {data.projects.map(project => (
+                    <li>
+                      <div className="project">
+                        <div className="row">
+                          <span className="name">{project.name}</span>
+                        </div>
+                        <div className="row">
+                          <div style={{display: 'flex'}}>
+                            <div style={{paddingRight: '10px'}}>
+                              {project.github && <IconLink text='Github' icon='fa-brands fa-github' link={project.github} />}
+                            </div>
+                            <div>
+                              {project.website && <IconLink text={project.website} icon='fa-solid fa-globe' link={"https://" + project.website} />}
+                            </div>
+                          </div>
+                          <div>
+                            {[...project.languages].map((lang: string) => <Language key={lang} lang={lang} />)}
+                          </div>
+                        </div>
+                        {project.description.length > 0 &&
+                          <ul>
+                            {project.description.map((dsc: string, i) => <li key={i}><span>{dsc}</span></li>)}
+                          </ul>
+                        }
+                      </div>
+                    </li>
+                  ))}
+                </ul>
+              </div>
             }
+
+
+            {/* experience  */}
             {data.work.length > 0 && data.work[0].position &&
-              <SectionL name='Experience'>
-                {data.work.map(w => <Work key={w.id} work={w} />)}
-              </SectionL>
+              <div className="section inline">
+                <h4>Experience</h4>
+                <ul style={{ listStyle: "none", padding: 0 }}>
+                  {data.work.map(work => (
+                    <li>
+                      <div className='work'>
+                        <div className='row'>
+                          <span className='pos'>{work.position}</span>
+                          <span>{work.start && parseDate(work.start) + " - " + ((work.end != undefined && work.end != "") ? parseDate(work.end) : "present")}</span>
+                        </div>
+                        <div className='row'>
+                          <span>{work.company}</span>
+                          <span>{work.location}</span>
+                        </div>
+                        <ul>
+                          {work.description.map((dsc: string) => <li><span>{dsc}</span></li>)}
+                        </ul>
+                      </div>
+                    </li>
+                  ))}
+                </ul>
+              </div>
             }
-          </Col>
-        </Main>
-      </Resume>
-    </>
+
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
