c5301d2dd1f48d14d0a22b2be7da859c829f1434
hypertensiune <dariuscirneala05@gmail.com>
2025-03-05 14:32:42 +0200

    config

---
+++ src/services/firebaseConfig.ts
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyAODMBCi2r0Xk7n-4JKHij9fhXb_T6b1t4",
+  authDomain: "leresume-68cb9.firebaseapp.com",
+  projectId: "leresume-68cb9",
+  storageBucket: "leresume-68cb9.firebasestorage.app",
+  messagingSenderId: "694315870755",
+  appId: "1:694315870755:web:31147721be054ad3a05f96"
+};
+
+export default firebaseConfig;
